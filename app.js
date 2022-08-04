eb47a0541761b227312dc7a3feed7002636dca6e
hasselGOP <hassel.ortegapelaez@gmail.com>
2022-08-03 22:55:41 -0500

    simplificando programa

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/// PROGRAMA QUE CAPTURA EL PEDIDO DEL CLIENTE
+/*/// PROGRAMA QUE CAPTURA EL PEDIDO DEL CLIENTE
 
 let restauranteInicio = alert("Restaurante Cazuela del antojo CDMX!");
 let restauranteBienvenida = alert("Bienvenid@ al sistema en linea del restaurante");
@@ -73,4 +73,46 @@ function pedido() {
             alert("Gracias por elegirnos, vuelve pronto")
         }      
 }
-pedido();
+pedido();*/
+
+//Funciones
+function menu(opcionSeleccionada) {
+    switch (opcionSeleccionada) {
+      case 1:
+        console.log("1", opcionSeleccionada);
+        cuantos("Enchiladas", 90);
+        break;
+      case 2:
+        // cuantos queres?
+        // calculo el valor
+        // informo lo que pidió
+        console.log("2", opcionSeleccionada);
+        cuantos("Quesadillas", 100);
+        break;
+      default: 
+        alert("Ingresó una opcion invalida");
+    }
+  }
+  
+  function cuantos(nombreDelProducto, precioDelProducto) {
+    let cantidad = prompt("¿Cuántas unidades de " + nombreDelProducto + " desea?")
+    let precio = cantidad * precioDelProducto; // calculo el valor
+    // informo lo que pidió
+    alert("Seleccionó " + cantidad + " " + nombreDelProducto + " y el total a pagar es " + precio);
+  }
+  
+  //Variables
+  let opcion;
+  
+  // PROGRAMA PRINCIPAL
+  // Pido opción a comprar
+  opcion = parseInt(prompt("¿Qué deseas pedir?(Ingrese solamente un numero de las siguientes opciones):\n 1-Enchiladas\n 2-Quesadillas \n0- FINALIZAR"));
+  while (opcion !== 0) { 
+  
+    menu(opcion)
+    // Pido opción a comprar
+    opcion = parseInt(prompt("¿Qué deseas pedir?(Ingrese solamente un numero de las siguientes opciones):\n 1-Enchiladas\n 2-Quesadillas \n0- FINALIZAR"));
+  }
+  
+  // Agradezco
+  alert("Gracias por su visita!");
